268d6233e747ac12b05ec58cc3d55517cb2be834
Gabriel <gopxfs@hotmail.com>
2022-07-04 14:50:20 -0300

    Add styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+// react
 import React from "react";
 import ReactDOM from "react-dom";
+
+// components
 import TodoContainer from './components/TodoContainer';
+
+//stylesheet
+import "./App.css";
 
 ReactDOM.render(
 <React.StrictMode><TodoContainer /></React.StrictMode>, document.getElementById('root')
